f50ca62af76ee2d6ef044baf39024c8abc194469
Michael Dewberry <michael.dewberry@gmail>
2017-12-10 21:44:31 -0500

    Declare RAM regions

--- src/main.c
+++ src/main.c
@@ -107,6 +107,12 @@ __attribute__((__section__(".flash_nvram")))
 static nvram_data_t flashy;
 #else
 nvram_data_t flashy;
+#ifdef DECLARE_NVRAM
+DECLARE_NVRAM(&flashy, sizeof(nvram_data_t))
+#endif
+#ifdef DECLARE_VRAM
+DECLARE_VRAM(&m, sizeof(mp_set))
+#endif
 #endif
 
 
